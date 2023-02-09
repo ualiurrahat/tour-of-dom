c683b483ff51598c5ce2e4dc12f0de4fa9a7815d
ualiurrahat <ualiurrahat25@gmail.com>
2023-02-09 14:02:46 +0600

    explored getElementByTagName,Id,ClassName,querySelector,querySelectorAll

---
+++ js/DOM.js
@@ -0,0 +1,38 @@
+console.log("hello rahat");
+console.log(document);
+console.log(document.body);
+
+//  <!-- we can get html elements usually in 5 ways  -->
+
+// option 1: getElementsByTagName();
+const liCollection = document.getElementsByTagName("li");
+console.log(liCollection);
+console.log(liCollection[0].innerText);
+for(const li of liCollection)
+{
+    console.log(li);
+}
+
+// option 2: get elements by class name
+const places = document.getElementsByClassName("must-go-places");
+console.log(places);
+for(const place of places)
+{
+    console.log(place.innerText);
+}
+// option 3 : by id 
+const examId = document.getElementById("exam-title");
+console.log(examId);
+
+// option 4: querySelectorAll()
+const placeSection = document.querySelectorAll(".place-container");
+console.log(placeSection);
+const locations = document.querySelectorAll(".place-container li");
+for(const location of locations)
+{
+    console.log(location.innerText);
+}
+// option 5 : querySelector()  ........select only first element
+const loc = document.querySelector(".place-container li");
+console.log(loc);
+
